ba1119a5dd7aa57614b69f390d0cac2113c45b43
Robert Bagwell <erbagwell@yahoo.com>
2026-07-14 16:10:53 -0600

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Manuscript Tracker</h1>
+      <p>Frontend scaffold is running.</p>
+    </div>
+  );
+}
+
+export default App;
